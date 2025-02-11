5d867be07fc4ab874e920e32694e8530c200a5e6
unknown <rommelprata@hotmail.com>
2025-02-11 11:37:07 -0500

    commit inicial

---
+++ src/main/java/com/javarom/agendamento_api/AgendamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.javarom.agendamento_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javarom/agendamento_api/AgendamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarom.agendamento_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
